f6bcdc70ecf41d044fed445f7217626012367ff7
mohit <connectwithlastdev@gmail.com>
2023-05-20 09:27:07 +0530

    router setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import Home from "./components/Home";
+import Header from './components/Header';
+import About from './components/About';
+import NoPage from './components/NoPage';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 
 function App() {
   return (
     <>
-    
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route exact path="/about" element={<About />} />
+          <Route exact path="/" element={<Home />} />
+          <Route path="*" element={<NoPage />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
